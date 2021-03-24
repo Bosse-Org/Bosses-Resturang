d2301e7bb33e63b8726d1fe3ff21b1b43e431f0c
= <=>
2021-03-24 08:57:14 +0100

    php

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+require("header.php");
+renderHeader("Home");
+?>
+    <div id="main">
+    </div>
+    <div id="footer">
+        <div id="leftFooter">
+            
+        </div>
+        <div id="rightFooter">
+            
+        </div>
+    </div>
+    <script src="main.js"></script>
+</body>
+
+</html>
