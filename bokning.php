1c9226ddab8d034f489c28bd8d1a6262c9a724ee
stridentorb <emil.nylander.1@vbu.se>
2021-03-24 08:59:33 +0100

    waw3

--- bokning.php
+++ bokning.php
@@ -9,8 +9,10 @@ renderHeader("Booking");
                 Förnamn:<br><input type="text" name="fName"><br>
                 Efternamn:<br><input type="text" name="lName"><br>
                 Telefonnummer:<br><input type="tel" name="pNumber"><br>
-                Antal personer:<br><input type="text" name="aPeople"><br>
-                <input type="submit" value="Submit"><br>
+                Tid:
+                <input id="Time" type="time" name="Time">
+                Antal personer:<input id="aPeople" type="text" name="aPeople"><br>
+                <input id="Boka" type="submit" value="Boka"><br>
             </form>
         </div>
     </div>
