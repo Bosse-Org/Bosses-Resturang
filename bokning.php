a030f22cc0f9b7ac96453f5346746e02020872f9
stridentorb <emil.nylander.1@vbu.se>
2021-04-16 12:08:59 +0200

    1.58

--- bokning.php
+++ bokning.php
@@ -1,6 +1,5 @@
 <?php
 require("include/CApp.php");
-$app->renderHeader("Bokning");
 
 class CBooking
 {
@@ -48,7 +47,26 @@ class CBooking
     public function renderBooking()
     {
         ?>
-        
+        <!DOCTYPE html>
+        <head>
+            <meta charset="UTF-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <title><?php echo($title)?></title>
+            <link href="https://fonts.googleapis.com/css2?family=Antonio" rel="stylesheet">
+            <link href="https://fonts.googleapis.com/css?family=Lobster" rel="stylesheet">
+            <link rel="stylesheet" href="styles/general.css">
+            <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.1/css/all.css" integrity="sha384-vp86vTRFVJgpjF9jiIGPEEqYqlDwgyBgEF109VFjmqGmIY/Y4HV4d3Gp2irVfcrp" crossorigin="anonymous"/>
+        </head>
+        <body>
+        <div id="menu">
+                <a href="index.php">Hem</a> 
+                <a href="bokning.php">Bokning</a>
+                <a>Om Oss</a>
+                <a>pog</a>
+                <a>pog</a>
+            </div>
+            
+            <div id="main">
         <div id="formdiv">
             <h2 class="booking-title">Bokning</h2>
             <form id="bookingForm" name="bookingForm" onsubmit="return validateForm(this);" method="post">
@@ -92,6 +110,7 @@ class CBooking
             </div>
             </form>
         </div>
+		</div>
         <?php
     }
 
