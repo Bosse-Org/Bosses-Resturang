d7c4db497a761d725dd0d5606f4f2bccaf540c05
ReactionAndy <svartakon@gmail.com>
2021-04-16 12:08:17 +0200

    d

--- index.php
+++ index.php
@@ -3,6 +3,10 @@ require_once("include/CApp.php");
 $app->renderHeader("Bosses Restaurang");
 ?>
 
+<div>
+    <img src="https://infobattle.org/images/space-lasers.png">
+</div>
+
 <?php
 $app->renderFooter();
 ?>
