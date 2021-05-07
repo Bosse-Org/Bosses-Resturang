b0f19fe6d3dc68de140d4435d049b9dff336ab84
stridentorb <emil.nylander.1@vbu.se>
2021-05-07 12:13:45 +0200

    1.555555

--- index.php
+++ index.php
@@ -3,7 +3,9 @@ require_once("include/CApp.php");
 $app->renderHeader("Bosses Restaurang");
 ?>
 <div id="main">
-    <img id="sushi" src="images/sushiplaceholder.jpg" width="800px">
+    <div id="sushi">
+    <img src="images/sushiplaceholder.jpg" width="800px">
+    </div>
 </div>
 <?php
 $app->renderFooter();
