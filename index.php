9f58c1a7cbf4c40df31232c5baa1b7776f77429e
stridentorb <emil.nylander.1@vbu.se>
2021-05-28 14:30:39 +0200

    1.57

--- index.php
+++ index.php
@@ -3,13 +3,6 @@ require_once("include/CApp.php");
 $app->renderHeader("Bosses Restaurang");
 ?>
     <div id="oxfile">
-<<<<<<< HEAD
-=======
-    <img src="images/oxfileplaceholder.png" width="800px"><br>
-    </div>
-    <div id="oxfile">
-    <img src="images/butter-burgers.jpg">
->>>>>>> 717cbfaec5f3fed5a079c461caa31af8db5c9ffc
     </div>
 <?php
 $app->renderFooter();
