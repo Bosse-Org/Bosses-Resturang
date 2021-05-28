717cbfaec5f3fed5a079c461caa31af8db5c9ffc
ReactionAndy <svartakon@gmail.com>
2021-05-28 09:24:57 +0200

    big bug :tf: u mad??

--- index.php
+++ index.php
@@ -3,7 +3,10 @@ require_once("include/CApp.php");
 $app->renderHeader("Bosses Restaurang");
 ?>
     <div id="oxfile">
-    <img src="images/oxfileplaceholder.png" width="800px">
+    <img src="images/oxfileplaceholder.png" width="800px"><br>
+    </div>
+    <div id="oxfile">
+    <img src="images/butter-burgers.jpg">
     </div>
 <?php
 $app->renderFooter();
